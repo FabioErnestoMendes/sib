737e4d8713a0ee26c313ed8300805eecb60ae8f0
Fabio Ernesto Mendes <mendesfabioernesto13@gmai.com>
2026-09-22 09:59:41 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,12 +1,12 @@
 __author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira",]
+__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Diogo Cachetas"
 __email__ = "diogo.cachetas@inl.int"
 
 __author__ = "Fábio Mendes" 
-__credits__ = ["Fábio Mendes"]
+__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira","Fábio Mendes"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Fábio Mendes"
